9cc2be0d532a9ed962c65d12a411a9d38ff7a6e8
Jairo <95053904+JairoAllende@users.noreply.github.com>
2024-06-27 19:32:56 -0300

    Add files via upload

--- PedidosYa/src/ar/edu/unlam/pb1/dominio/Carrito.java
+++ PedidosYa/src/ar/edu/unlam/pb1/dominio/Carrito.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Carrito {
 	private static Scanner teclado = new Scanner(System.in);
-	
-	private Producto[] carrito;
+
+	private Producto[] carrito = new Producto[1000];
 	private double saldo;
-	
+
 	public Carrito(int espacioCarrito) {
 		this.carrito = new Producto[espacioCarrito];
 		this.saldo = 0;
@@ -16,69 +16,206 @@ public class Carrito {
 	public void agregarAlCarrito(Producto productoAgregado, int cantidadElejida) {
 		boolean agregado = false;
 		for (int i = 0; i < carrito.length; i++) {
-			if(carrito[i] == null && agregado == false) {
-				if(productoAgregado.getCantidad() >= cantidadElejida) {
+			if (carrito[i] == null && agregado == false) {
+				if (productoAgregado.getCantidad() >= cantidadElejida) {
 					productoAgregado.setCantidad(productoAgregado.getCantidad() - cantidadElejida);
-					Producto productoAgregadoAlCarrito = new Producto(productoAgregado.getNombre(), productoAgregado.getPrecio());
+					Producto productoAgregadoAlCarrito = new Producto(productoAgregado.getNombre(),
+							productoAgregado.getPrecio());
 					productoAgregadoAlCarrito.setCantidad(cantidadElejida);
 					carrito[i] = productoAgregadoAlCarrito;
 					agregado = true;
-					saldoAcumulado(productoAgregadoAlCarrito);
+					saldoAcumulado(productoAgregadoAlCarrito, true);
 				}
 			}
 		}
 	}
-	
-	private void saldoAcumulado(Producto productoAgregado) {
+
+	private void saldoAcumulado(Producto productoAgregado, boolean agregar) {
 		double precioDelProducto = productoAgregado.getPrecio();
 		int cantidad = productoAgregado.getCantidad();
 		double subtotal = precioDelProducto * cantidad;
-		this.saldo += subtotal;
+		if (agregar == true) {
+			this.saldo += subtotal;
+		} else {
+			this.saldo -= subtotal;
+		}
 	}
-	
-	public void verCarrito() {
-		int opcion;
-		do {
-			for (int i = 0; i < carrito.length; i++) {
-				if(carrito[i] != null) {
-					System.out.println((i+1) +  ". " + carrito[i]);
-				}
+
+	public void sacarDelCarrito(Producto productoEnCarrito, int opcion) {
+		
+		System.out.println("Cuántos elementos de este producto desea eliminar del carrito?");
+		int cantidadAEliminar = teclado.nextInt();
+
+		if (productoEnCarrito.getCantidad() >= cantidadAEliminar) {
+			Producto productoAEliminar = new Producto(productoEnCarrito.getNombre(), productoEnCarrito.getPrecio());
+			productoAEliminar.setCantidad(cantidadAEliminar);
+			saldoAcumulado(productoAEliminar, false);
+
+			productoEnCarrito.setCantidad(productoEnCarrito.getCantidad() - cantidadAEliminar);
+			if (productoEnCarrito.getCantidad() == 0) {
+				carrito[opcion] = null;
 			}
-			System.out.println("\nIngrese 0 para volver o 1 para elimiar elementos del carrito");
-			opcion = teclado.nextInt();
-			if(opcion == 1) {
-				sacarDelCarrito();
-			}
-			
-		}while(opcion != 0);
-	}
-	
-	public void sacarDelCarrito() { 
-		int opcion;
-		System.out.println("Ingrese el indice del producto que desea sacar");
-		opcion = teclado.nextInt();
-		if(carrito[opcion - 1] != null) {
-			System.out.println("Cuantos elementos de este producto desea eliminar del carrito?");
-			opcion = teclado.nextInt();
-			if(carrito[opcion - 1].getCantidad() > opcion) {
-				carrito[opcion - 1].setCantidad(carrito[opcion - 1].getCantidad() - opcion); // ingreso la opcion como indice 
-				System.out.println("Se eliminaron " + opcion + " elementos");
-			}else if(carrito[opcion - 1].getCantidad() == opcion) {
-				
-				System.out.println("Se eliminaron todos los elementos de tipo " + carrito[opcion - 1].getNombre());
-				carrito[opcion - 1] = null;
-			}else {
-				System.out.println("Tiene menos cantdad de elementos de la que desea eliminar");
-			}
+
+			System.out.println(
+					"Se eliminaron " + cantidadAEliminar + " elementos de " + productoEnCarrito.getNombre() + "\n");
+			System.out.println("El saldo acumulado es de: " + getSaldo() + "\n");
+		} else {
+			System.out.println("Tiene menos cantidad de elementos de la que desea eliminar.");
 		}
 	}
 	
+	public void agregarElementoAlCarrito(Producto productoEnCarrito, int opcion) {
+		
+		System.out.println("Cuántos elementos de este producto desea agregar al carrito?");
+		int cantidadAAgregar = teclado.nextInt();
+		productoEnCarrito.setCantidad(productoEnCarrito.getCantidad() + cantidadAAgregar);
+		
+		double precioDelProducto = productoEnCarrito.getPrecio();
+		this.saldo += precioDelProducto * cantidadAAgregar;
+		
+		System.out.println("Se agregaron " + cantidadAAgregar + " elementos de " + productoEnCarrito.getNombre() + "\n");
+		
+		System.out.println("El saldo acumulado es de: " + getSaldo() + "\n");
+	}
+
+	public void verCarrito() {
+		int opcion;
+		do {
+			System.out.println("\nEl saldo de tu cuenta es de:" + Usuario.getSaldoEnLaCuenta());
+			for (int i = 0; i < carrito.length; i++) {
+				if (carrito[i] != null) {
+					System.out.println((i + 1) + ". " + carrito[i]);
+				}
+			}
+			System.out.println("\nEl saldo acumulado es de: " + getSaldo());
+			System.out.println(
+					"\nOPCIONES DEL CARRITO:\n0. Para volver\n1. Para mostrar un producto especifico segun el indice\n2. Para ordenar precios de mayor a menor\n3. Para ordenar precios de menor a mayor \n4. Mostrar por rango de precios\n5. Pagar");
+			opcion = teclado.nextInt();
+			switch (opcion) {
+			case 0:
+				break;
+			case 1:
+				mostrarProductoEnPosicion();
+				break;
+			case 2:
+				ordenarPorPrecioMayorAMenor();
+				break;
+			case 3:
+				ordenarPorPrecioMenorAMayor();
+				break;
+			case 4:
+				mostrarProductosEnRangoDePrecios();
+				break;
+			case 5:
+				pagar();
+				break;
+			default:
+				System.out.println("opcion incorrecta");
+				break;
+			}
+
+		} while (opcion != 0);
+	}
+
+	public void ordenarPorPrecioMayorAMenor() {
+		for (int i = 0; i < carrito.length - 1; i++) {
+			for (int j = 0; j < carrito.length - 1 - i; j++) {
+				if (carrito[j] != null && carrito[j + 1] != null
+						&& carrito[j].getPrecio() < carrito[j + 1].getPrecio()) {
+					Producto temp = carrito[j];
+					carrito[j] = carrito[j + 1];
+					carrito[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	public void ordenarPorPrecioMenorAMayor() {
+		for (int i = 0; i < carrito.length - 1; i++) {
+			for (int j = 0; j < carrito.length - 1 - i; j++) {
+				if (carrito[j] != null && carrito[j + 1] != null
+						&& carrito[j].getPrecio() > carrito[j + 1].getPrecio()) {
+					Producto temp = carrito[j];
+					carrito[j] = carrito[j + 1];
+					carrito[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	public void mostrarProductosEnRangoDePrecios() {
+
+		System.out.print("Ingrese el precio mínimo: ");
+		double precioMinimo = teclado.nextDouble();
+
+		System.out.print("Ingrese el precio máximo: ");
+		double precioMaximo = teclado.nextDouble();
+
+		boolean hayProductosEnRango = false;
+
+		System.out.println("\nProductos en el rango de precios " + precioMinimo + " - " + precioMaximo + ":");
+		for (int i = 0; i < carrito.length; i++) {
+			if (carrito[i] != null) {
+				double precio = carrito[i].getPrecio();
+				if (precio >= precioMinimo && precio <= precioMaximo) {
+					System.out.println(carrito[i]);
+					hayProductosEnRango = true;
+				}
+			}
+		}
+
+		if (!hayProductosEnRango) {
+			System.out.println("No hay productos en ese rango de precio");
+		}
+
+		System.out.println("\nIngrese 0 para volver a las opciones del carrito");
+		int volverAOpcionesDelCarrito = teclado.nextInt();
+		if (volverAOpcionesDelCarrito == 0) {
+			verCarrito();
+		}
+	}
+
+	public void mostrarProductoEnPosicion() {
+
+		System.out.print("\nIngrese la posición del producto que desea mostrar: ");
+		int posicion = teclado.nextInt();
+		posicion = posicion - 1;
+
+		if (posicion >= 0 && posicion < carrito.length && carrito[posicion] != null) {
+			System.out.println("Producto en la posición " + (posicion + 1) + ":");
+			System.out.println(carrito[posicion] + "\n");
+			
+			Producto productoEnCarrito = carrito[posicion];
+			System.out.println("Ingrese 1 si desea agregar elementos de este producto\nIngrese 2 si desea eliminar elementos de este producto\nIngrese 0 para volver a las opciones del carrito");
+			int opcion = teclado.nextInt();
+			if(opcion == 1) {
+				agregarElementoAlCarrito(productoEnCarrito, posicion);
+			}else if(opcion == 2) {
+				sacarDelCarrito(productoEnCarrito, posicion);
+			}else if(opcion == 0) {
+				verCarrito();
+			}
+
+		} else {
+			System.out.println("No hay producto en la posición especificada.\n");
+		}
+	}
+	
+	public void pagar() {
+		if(Usuario.getSaldoEnLaCuenta() >= this.saldo) {
+			Usuario.setSaldoEnLaCuenta(Usuario.getSaldoEnLaCuenta() - (float)this.saldo);
+			System.out.println("EL PEDIDO SE REALIZO CON EXITO!");
+			for (int i = 0; i < carrito.length; i++) {
+				carrito[i] = null;
+			}
+			this.saldo = 0;
+		}else if(Usuario.getSaldoEnLaCuenta() < this.saldo) {
+			System.out.println("No cuenta con saldo suficiente en la cuenta");
+		}
+	}
+
 	public double getSaldo() {
 		return this.saldo;
 	}
-	
-	public Producto[] getCarrito() {
-		return this.carrito;
-	}	
-	
+
 }
--- PedidosYa/src/ar/edu/unlam/pb1/dominio/ManagerDeMenus.java
+++ PedidosYa/src/ar/edu/unlam/pb1/dominio/ManagerDeMenus.java
@@ -28,11 +28,10 @@ public class ManagerDeMenus {
 				manejarMenuTiposDeVerdulerias();
 				break;
 			case CARRITO:
-				System.out.println("El saldo acumulado es de: " + carrito.getSaldo());
 				carrito.verCarrito();
 				break;
 			case SALIR:
-				System.out.println("nos vemos");
+				System.out.println("Hasta la Proxima");
 				break;
 			}
 		}while(!opcion.equals(MenuPrincipal.SALIR));
@@ -56,11 +55,10 @@ public class ManagerDeMenus {
 			opcionTiposDeSupermercado = ingresarOpcionTiposDeSupermercados();
 			switch(opcionTiposDeSupermercado) {
 			case CARREFOUR:
-			case COTO:
 			case CHANGOMAS:
-			case DISCO:
+			case COTO:
 			case DIA:
-			case DIARCO:
+			case DISCO:
 				manejarMenuSupermercado();
 				break;
 			case VOLVER:
@@ -98,9 +96,6 @@ public class ManagerDeMenus {
 			case VARIOS:
 				manejarMenuVarios();
 				break;
-			case PROMOCIONES:
-				System.out.println("Promociones");
-				break;
 			case VOLVER:
 				return;
 			}
@@ -128,9 +123,17 @@ public class ManagerDeMenus {
 				manejarComidasListas();
 				break;
 			case GALLETITAS_Y_PANADERIAS:
+				manejarGalletitasYPanaderias();
+				break;
 			case LACTEOS_Y_QUESOS:
+				manejarLacteosYQuesos();
+				break;
 			case PASTAS:
+				manejarPastas();
+				break;
 			case SNACKS:
+				manejarSnacks();
+				break;
 			case VOLVER:
 				return;
 			}
@@ -155,10 +158,16 @@ public class ManagerDeMenus {
 			opcionMenuBebidas = ingresarOpcionDelMenuBebidas();
 			switch(opcionMenuBebidas) {
 			case AGUA:
+				manejarAgua();
+				break;
 			case AGUA_SABORIZADA:
+				manejarAguaSaborizada();
+				break;
 			case BEBIDAS_ALCOHOLICAS:
+				manejarBebidasAlcoholicas();
+				break;
 			case GASEOSAS:
-				System.out.println("bebidas");
+				manejarGaseosas();
 				break;
 			case VOLVER:
 				return;
@@ -184,10 +193,16 @@ public class ManagerDeMenus {
 			opcionMenuLimpieza = ingresarOpcionDelMenuLimpieza();
 			switch(opcionMenuLimpieza) {
 			case ESCOBA:
+				manejarEscoba();
+				break;
 			case MOPA:
+				manejarMopa();
+				break;
 			case LAVANDINA:
+				manejarLavandina();
+				break;
 			case SUAVIZANTE:
-				System.out.println("Limpieza");
+				manejarSuavizante();
 				break;
 			case VOLVER:
 				return;
@@ -213,9 +228,13 @@ public class ManagerDeMenus {
 			opcionMenuVarios = ingresarOpcionDelMenuVarios();
 			switch(opcionMenuVarios) {
 			case BAZAR:
+				manejarBazar();
+				break;
 			case JUGUETERIA:
+				manejarJugueteria();
+				break;
 			case LIBREBIA:
-				System.out.println("Varios");
+				manejarLibreria();
 				break;
 			case VOLVER:
 				return;
@@ -271,12 +290,18 @@ public class ManagerDeMenus {
 			mostrarMenuKiosco();
 			opcionMenuKiosco = ingresarOpcionDelMenuKiosco();
 			switch(opcionMenuKiosco) {
-			case ALFAJORES: 
+			case ALFAJORES:
+				manejarAlfajores();
+				break;
 			case CARAMELO_Y_CHICLES: 
-			case BARRITA_DE_CEREAL: 
+				manejarCarameloYChicles();
+				break;
+			case BARRITA_DE_CEREAL:
+				manejarBarritasDeCereal();
+				break;
 			case BEBIDAS:
-			case PROMOCIONES:
-				System.out.println("COMIDASKIOSCO");
+				manejarBebidas();
+				break;
 			case VOLVER:
 				return;
 			}
@@ -328,13 +353,17 @@ public class ManagerDeMenus {
 			mostrarMenuCarniceria();
 			opcionMenuCarniceria = ingresarOpcionDelMenuCarniceria();
 			switch(opcionMenuCarniceria) {
-			case ASADO:
+			case VACUNA:
+				manejarVacuna();
 				break;
-			case CHURRASCO:
+			case CERDO:
+				manejarCerdo();
 				break;
-			case CHORIZO:
+			case EMBUTIDOS:
+				manejarEmbutidos();
 				break;
 			case POLLO:
+				manejarPollo();
 				break;
 			case VOLVER:
 				return;
@@ -388,11 +417,14 @@ public class ManagerDeMenus {
 			mostrarMenuVerduleria();
 			opcionMenuVerduleria = ingresarOpcionDelMenuVerduleria();
 			switch(opcionMenuVerduleria) {
-			case PAPA:
-			case LECHUGA:
-			case TOMATE:
-			case ZANAHORIA:
-				System.out.println("ok");
+			case FRUTAS:
+				manejarFrutas();
+				break;
+			case VERDURAS:
+				manejarVerduras();
+				break;
+			case HUEVOS:
+				manejarHuevos();
 				break;
 			case VOLVER:
 				return;
@@ -411,6 +443,8 @@ public class ManagerDeMenus {
 		return MenuVerduleria.values()[teclado.nextInt() - 1];
 	}
 	
+	/////////////
+	
 	private static void manejarComidasListas() {
 		Producto[] listaDeProductosDisponibles = new Producto[2];
 		Producto burrito = new Producto("Burritos Integrales", 4825);
@@ -420,25 +454,291 @@ public class ManagerDeMenus {
 		agregarProductosAlCarrito(listaDeProductosDisponibles);
 	}
 	
-	public static void agregarProductosAlCarrito(Producto[] listaDeProductosDisponibles) {
-		int opcion;
-		int cantidad;
-		do {
-			for (int i = 0; i < listaDeProductosDisponibles.length; i++) {
-				System.out.println((i+1) + ". " + listaDeProductosDisponibles[i].toString());
-			}
-			System.out.println("\nIngrese el numero del indice para añadir el producto al carrito o 0 para volver");
-			opcion = teclado.nextInt();
-			if(opcion == 1) {
-				System.out.println("Ingrese la cantidad");
-				cantidad = teclado.nextInt();
-				carrito.agregarAlCarrito(listaDeProductosDisponibles[0], cantidad);
-			}else if(opcion == 2) {
-				System.out.println("Ingrese la cantidad");
-				cantidad = teclado.nextInt();
-				carrito.agregarAlCarrito(listaDeProductosDisponibles[1], cantidad);
-			}
-		}while(opcion != 0);
+	private static void manejarGalletitasYPanaderias() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto galletitasOreo = new Producto("Galletitas Oros", 1060);
+		Producto panDeHamburguesas = new Producto("Pan de Hamburguesa", 2360);
+		listaDeProductosDisponibles[0] = galletitasOreo;
+		listaDeProductosDisponibles[1] = panDeHamburguesas;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
 	}
+	
+	private static void manejarLacteosYQuesos() {
+		Producto[] listaDeProductosDisponibles = new Producto[3];
+		Producto lecheLaSerenisimaDescremada = new Producto("Leche LaSerenisima Descremada 1Lt", 1979);
+		Producto lecheChocolatadaCindor = new Producto("Leche Chocolatada Cindor 1Lt", 3698);
+		Producto huevosColorDocena = new Producto("Huevos Color x12", 3400);
+		listaDeProductosDisponibles[0] = lecheLaSerenisimaDescremada;
+		listaDeProductosDisponibles[1] = lecheChocolatadaCindor;
+		listaDeProductosDisponibles[2] = huevosColorDocena;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarPastas() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto raviolesDeVerduraYQueso = new Producto("Ravioles de Verdula y Queso", 1600);
+		Producto ñoquis = new Producto("Ñoquis 500g", 1885);
+		listaDeProductosDisponibles[0] = raviolesDeVerduraYQueso;
+		listaDeProductosDisponibles[1] = ñoquis;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarSnacks() {
+		Producto[] listaDeProductosDisponibles = new Producto[3];
+		Producto papasFritasLays = new Producto("Papas Fritas Lays 85g", 2435);
+		Producto saladix = new Producto("Saladix 30g", 680);
+		Producto rex = new Producto("Galletitas Rex", 1030);
+		listaDeProductosDisponibles[0] = papasFritasLays;
+		listaDeProductosDisponibles[1] = saladix;
+		listaDeProductosDisponibles[2] = rex;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	//////////
+	
+	private static void manejarAgua() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto aguaVillavicencio = new Producto("Agua Villaviencio", 1200);
+		Producto aguaSmartWater = new Producto("Agua SmartWater", 900);
+		listaDeProductosDisponibles[0] = aguaVillavicencio;
+		listaDeProductosDisponibles[1] = aguaSmartWater;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarAguaSaborizada() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto aquarius = new Producto("Aquarius", 1650);
+		Producto levite = new Producto("Levite", 1740);
+		listaDeProductosDisponibles[0] = aquarius;
+		listaDeProductosDisponibles[1] = levite;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarBebidasAlcoholicas() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto cerveza = new Producto("Cerveza Quilmes", 2200);
+		Producto fernetBranca = new Producto("Fernet Branca", 9300);
+		listaDeProductosDisponibles[0] = cerveza;
+		listaDeProductosDisponibles[1] = fernetBranca;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarGaseosas() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto coca = new Producto("Coca-Cola", 3100);
+		Producto sevenUp = new Producto("7Up", 2300);
+		listaDeProductosDisponibles[0] = coca;
+		listaDeProductosDisponibles[1] = sevenUp;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	//////////
+	
+	private static void manejarEscoba() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto escoba = new Producto("Escoba" , 1900);
+		Producto escobillon = new Producto("Escobillon" , 3000);
+		listaDeProductosDisponibles[0] = escoba;
+		listaDeProductosDisponibles[1] = escobillon;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarMopa() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto mopa = new Producto("Mopa" , 11000);
+		Producto mopaYBalde = new Producto("Mopa + Balde" , 25000);
+		listaDeProductosDisponibles[0] = mopa;
+		listaDeProductosDisponibles[1] = mopaYBalde;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarLavandina() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto lavandinaChico = new Producto("Lavandina 1Lt" , 2000);
+		Producto lavandinaGrande = new Producto("Lavandina 4Lt" , 5300);
+		listaDeProductosDisponibles[0] = lavandinaChico;
+		listaDeProductosDisponibles[1] = lavandinaGrande;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarSuavizante() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto suavizanteChico = new Producto("Suavizante 500ml", 2380);
+		Producto suavizanteGrande = new Producto("Suavizante 3Lt" , 7600);
+		listaDeProductosDisponibles[0] = suavizanteChico;
+		listaDeProductosDisponibles[1] = suavizanteGrande;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	//////////
+	
+	private static void manejarBazar() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto platos = new Producto("Set de platos", 7200);
+		Producto escarbadientes = new Producto("Escarbadientes", 890);
+		listaDeProductosDisponibles[0] = platos;
+		listaDeProductosDisponibles[1] = escarbadientes;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarJugueteria() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto peluche = new Producto("Peluche Pikachu", 12300);
+		Producto soldadito = new Producto("Soldadito", 5000);
+		listaDeProductosDisponibles[0] = peluche;
+		listaDeProductosDisponibles[1] = soldadito;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarLibreria() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto libroHp = new Producto("Coleccion Saga Harry Potter", 32500);
+		Producto historieta = new Producto("Historieta", 9000);
+		listaDeProductosDisponibles[0] = libroHp;
+		listaDeProductosDisponibles[1] = historieta;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	//////////
+	
+	private static void manejarAlfajores() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto alfajorCapitanDelEspacio = new Producto("Capitan Del Espacio", 1000);
+		Producto alfajorGuaymallen = new Producto("Guaymallen", 800);
+		listaDeProductosDisponibles[0] = alfajorCapitanDelEspacio;
+		listaDeProductosDisponibles[1] = alfajorGuaymallen;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarCarameloYChicles() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto caramelo = new Producto("Butter Toffee", 100);
+		Producto chicleBeldent = new Producto("Chicle Beldent", 600);
+		listaDeProductosDisponibles[0] = caramelo;
+		listaDeProductosDisponibles[1] = chicleBeldent;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarBarritasDeCereal() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto barrita = new Producto("CerealMix", 1140);
+		Producto barritaCerealFort = new Producto("CerealFort", 550);
+		listaDeProductosDisponibles[0] = barrita;
+		listaDeProductosDisponibles[1] = barritaCerealFort;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarBebidas() {
+		Producto[] listaDeProductosDisponibles = new Producto[3];
+		Producto latitaCoca = new Producto("Latita Coca-Cola", 1200);
+		Producto latitaFanta = new Producto("Latita Fanta", 1200);
+		Producto latitaPepsi = new Producto("Latita Pepsi", 1000);
+		listaDeProductosDisponibles[0] = latitaCoca;
+		listaDeProductosDisponibles[1] = latitaFanta;
+		listaDeProductosDisponibles[2] = latitaPepsi;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	///////////////
+	
+	private static void manejarVacuna() {
+		Producto[] listaDeProductosDisponibles = new Producto[3];
+		Producto lomo = new Producto("Lomo 0.5Kg", 7105);
+		Producto bifeDeChorizo = new Producto("Bife de Chorizo 0.5Kg", 6975);
+		Producto paleta = new Producto("Paleta 0.5Kg", 5650);
+		listaDeProductosDisponibles[0] = lomo;
+		listaDeProductosDisponibles[1] = bifeDeChorizo;
+		listaDeProductosDisponibles[2] = paleta;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarCerdo() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto solomillo = new Producto("Solomillo 0.5Kg", 3500);
+		Producto pechitoDeCerdo = new Producto("Pechito de Cerdo 0.5Kg", 2100);
+		listaDeProductosDisponibles[0] = solomillo;
+		listaDeProductosDisponibles[1] = pechitoDeCerdo;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarEmbutidos() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto chorizoColorado = new Producto("Chorizo Colorado 0.5Kg", 4150);
+		Producto salchichaParrillera = new Producto("Salchicha Parrillera 0.5Kg", 4850);
+		listaDeProductosDisponibles[0] = chorizoColorado;
+		listaDeProductosDisponibles[1] = salchichaParrillera;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarPollo() {
+		Producto[] listaDeProductosDisponibles = new Producto[3];
+		Producto alitaDePollo = new Producto("Alita de Pollo 0.5Kg", 1050);
+		Producto supremaDePollo = new Producto("Suprema de Pollo 0.5Kg", 4945);
+		Producto polloEntero = new Producto("Pollo Entero 2.5Kg", 9750);
+		listaDeProductosDisponibles[0] = alitaDePollo;
+		listaDeProductosDisponibles[1] = supremaDePollo;
+		listaDeProductosDisponibles[2] = polloEntero;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	////////////////
+	
+	private static void manejarFrutas() {
+		Producto[] listaDeProductosDisponibles = new Producto[3];
+		Producto pera = new Producto("Pera xKg", 900);
+		Producto kiwi = new Producto("Kiwi x kg", 7000);
+		Producto manzana = new Producto("Manzana xKg", 3200);
+		listaDeProductosDisponibles[0] = pera;
+		listaDeProductosDisponibles[1] = kiwi;
+		listaDeProductosDisponibles[2] = manzana;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarVerduras() {
+		Producto[] listaDeProductosDisponibles = new Producto[3];
+		Producto tomate = new Producto("Tomate xKg", 4120);
+		Producto lechuga = new Producto("Lechuga x kg", 3050);
+		Producto zanahoria = new Producto("Zanahoria xKg", 1200);
+		listaDeProductosDisponibles[0] = tomate;
+		listaDeProductosDisponibles[1] = lechuga;
+		listaDeProductosDisponibles[2] = zanahoria;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	private static void manejarHuevos() {
+		Producto[] listaDeProductosDisponibles = new Producto[2];
+		Producto mapleDeHuevo = new Producto("Maple de Huevo", 5600);
+		Producto docenaDeHuevo = new Producto("Huevo Blanco Grande x12", 3500);
+		listaDeProductosDisponibles[0] = mapleDeHuevo;
+		listaDeProductosDisponibles[1] = docenaDeHuevo;
+		agregarProductosAlCarrito(listaDeProductosDisponibles);
+	}
+	
+	
+	public static void agregarProductosAlCarrito(Producto[] listaDeProductosDisponibles) {
+		
+	        int opcion;
+	        int cantidad;
+
+	        do {
+	            System.out.println("\nProductos disponibles:");
+	            for (int i = 0; i < listaDeProductosDisponibles.length; i++) {
+	                if (listaDeProductosDisponibles[i] != null) {
+	                    System.out.println((i + 1) + ". " + listaDeProductosDisponibles[i].toString());
+	                }
+	            }
+	            System.out.println("\nIngrese el número del índice para añadir el producto al carrito o 0 para volver:");
+	            opcion = teclado.nextInt();
+
+	            if (opcion > 0 && opcion <= listaDeProductosDisponibles.length && listaDeProductosDisponibles[opcion - 1] != null) {
+	                System.out.println("Ingrese la cantidad:");
+	                cantidad = teclado.nextInt();
+	                carrito.agregarAlCarrito(listaDeProductosDisponibles[opcion - 1], cantidad);
+	            } else if (opcion != 0) {
+	                System.out.println("Opción inválida. Intente de nuevo.");
+	            }
+
+	        } while (opcion != 0);
+	    }
 	
 }
