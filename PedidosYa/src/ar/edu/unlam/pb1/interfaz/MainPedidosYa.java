0c0869f4b86dc49e8bb73deb40fa3f6930547379
Jairo <95053904+JairoAllende@users.noreply.github.com>
2024-06-27 19:34:03 -0300

    Add files via upload

--- PedidosYa/src/ar/edu/unlam/pb1/interfaz/MainPedidosYa.java
+++ PedidosYa/src/ar/edu/unlam/pb1/interfaz/MainPedidosYa.java
@@ -1,14 +1,13 @@
 package ar.edu.unlam.pb1.interfaz;
 
 import ar.edu.unlam.pb1.dominio.ManagerDeMenus;
+import ar.edu.unlam.pb1.dominio.Usuario;
 
 public class MainPedidosYa {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Bienvenido!");
-		
-		
+		System.out.println("Bienvenido!\nEl saldo de tu cuenta es de: " + Usuario.getSaldoEnLaCuenta());
 		
 		ManagerDeMenus.manejarMenuPrincipal();
 	}
